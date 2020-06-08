ca6df12c1aa7c1f4f7558df6a59d04321b95585f
Rory Flint <roryflintphoto@gmail.com>
2020-06-08 17:35:17 +0100

    Work on test project to understand architecture

--- popup.js
+++
@@ -1,8 +0,0 @@
-console.log('this is popup.js speaking.');
-
-document.addEventListener('DOMContentLoaded', testFunc);
-
-function testFunc() {
-    const button = document.getElementById('#test123');
-    console.log(button);
-}
