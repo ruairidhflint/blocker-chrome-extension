1c6b8074465d05adf56a029cd437bfdbd00c1283
Rory Flint <roryflintphoto@gmail.com>
2020-06-08 11:38:01 +0100

    Primitive ability to redirect from unwanted site

--- background.js
+++ background.js
@@ -1,3 +1,9 @@
 const testHref = window.location.href;
 
+if (testHref.includes('https://www.google.com/')) {
+  window.location.href = 'https://rory.codes';
+} else {
+  console.log('This is fine!');
+}
+
 console.log(testHref);
