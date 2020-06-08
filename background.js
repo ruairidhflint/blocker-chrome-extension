eb08cb2373bbb5c42f3face3e4fb11f8882300c2
Rory Flint <roryflintphoto@gmail.com>
2020-06-08 20:42:41 +0100

    Huge progress

--- background.js
+++ background.js
@@ -1,26 +1,4 @@
-// const currentURL = window.location.href;
-
-const blockedSites = [
-  'https://www.dailymail.co.uk',
-  //   'https://www.reddit.com',
-];
-
-// for (let i = 0; i < blockedSites.length; i++) {
-//   if (currentURL.includes(blockedSites[i])) {
-//     window.location.href = 'https://rory.codes';
-//   } else {
-//     console.log('This is fine!');
-//   }
-// }
-
-// chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-//   chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
-//     let url = tabs[0].url;
-//     // use `url` here inside the callback because it's asynchronous!
-//     for (let i = 0; i < blockedSites.length; i++) {
-//       if (url && url.includes(blockedSites[i])) {
-//         chrome.tabs.update(null, 'https://rory.codes', null);
-//       }
-//     }
-//   });
-// });
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({blocked: "['https://www.dailymail.co.uk', 'https://rory.codes' ]"}, function() {
+    });
+  });
--- content.js
+++ content.js
@@ -1,13 +1,14 @@
 const currentURL = window.location.href;
+const optionsUrl = chrome.extension.getURL('blocked.html');
+const content = optionsUrl;
 
-const blockedSites = ['https://www.dailymail.co.uk'];
+let blockedSites;
 
-var optionsUrl = chrome.extension.getURL("blocked.html"); 
-var content = optionsUrl;
-
-for (let i = 0; i < blockedSites.length; i++) {
-  if (currentURL.includes(blockedSites[i])) {
-    window.location.href = content;
+chrome.storage.sync.get('blocked', function (data) {
+  blockedSites = data.blocked;
+  for (let i = 0; i < blockedSites.length; i++) {
+    if (currentURL.includes(blockedSites[i])) {
+      window.location.href = content;
+    }
   }
-}
-
+});
