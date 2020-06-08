d65bd2a0b15f216b33d78ff6937034b0b0eb8177
Rory Flint <roryflintphoto@gmail.com>
2020-06-08 20:52:15 +0100

    Fixed parsing bug

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({blocked: "['https://www.dailymail.co.uk', 'https://rory.codes' ]"}, function() {
+    chrome.storage.sync.set({blocked: '["https://rory.codes"]'}, function() {
     });
   });
--- content.js
+++ content.js
@@ -1,14 +1,18 @@
-const currentURL = window.location.href;
-const optionsUrl = chrome.extension.getURL('blocked.html');
+var currentURL = window.location.href;
+var optionsUrl = chrome.extension.getURL('blocked.html');
 const content = optionsUrl;
 
 let blockedSites;
 
 chrome.storage.sync.get('blocked', function (data) {
-  blockedSites = data.blocked;
-  for (let i = 0; i < blockedSites.length; i++) {
-    if (currentURL.includes(blockedSites[i])) {
-      window.location.href = content;
+  blockedSites = JSON.parse(data.blocked);
+  if (blockedSites.length) {
+    for (let i = 0; i < blockedSites.length; i++) {
+      if (currentURL.includes(blockedSites[i])) {
+        window.location.href = content;
+      } else {
+        console.log('hello');
+      }
     }
   }
 });
